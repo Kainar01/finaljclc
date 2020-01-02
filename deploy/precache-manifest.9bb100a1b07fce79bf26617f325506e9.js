7489879f470ee7bc4000395e13e3546439a392ee
kaini <53599628+Kainar01@users.noreply.github.com>
2020-01-02 22:18:32 +0600

    commit

---
+++ deploy/precache-manifest.9bb100a1b07fce79bf26617f325506e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d3f9cd088f2f7844d8caa714256ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332e9c0e9cfc2f89d731",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5f001051470a30c82943",
+    "url": "/static/css/main.7a5b278c.chunk.css"
+  },
+  {
+    "revision": "332e9c0e9cfc2f89d731",
+    "url": "/static/js/2.a319c21d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.a319c21d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f001051470a30c82943",
+    "url": "/static/js/main.e98899d4.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "21c7f50a2813a2b8a759f140cba097cf",
+    "url": "/static/media/HSK-logo.21c7f50a.jpg"
+  },
+  {
+    "revision": "addcc1a80f41280f1458d953de2d1a28",
+    "url": "/static/media/ch-bg.addcc1a8.jpg"
+  },
+  {
+    "revision": "e74daaaad32dc1331ff7d7b675235d6e",
+    "url": "/static/media/chinese-blackboard.e74daaaa.png"
+  },
+  {
+    "revision": "a946f5a271f9138c009b29cd0d444747",
+    "url": "/static/media/hands.a946f5a2.svg"
+  },
+  {
+    "revision": "6f133a7d8321639f2dd00cb0fb059206",
+    "url": "/static/media/home-background.6f133a7d.jpg"
+  },
+  {
+    "revision": "7774195e9998caaec4b5d4cada6dbfdb",
+    "url": "/static/media/jp-bg.7774195e.jpg"
+  },
+  {
+    "revision": "584781dd834f99c65bbb5dddf496d478",
+    "url": "/static/media/jp-ch1.584781dd.jpg"
+  },
+  {
+    "revision": "bc246fd7ba56f9c0d1616ee592b5df81",
+    "url": "/static/media/kanji1.bc246fd7.jpg"
+  },
+  {
+    "revision": "891a120306357be63ad9b5a4936ee5a9",
+    "url": "/static/media/kanji2.891a1203.jpg"
+  },
+  {
+    "revision": "89378ddbd8ad3aae1347911868f68799",
+    "url": "/static/media/level.89378ddb.jpg"
+  },
+  {
+    "revision": "7d31297d5c12a8335defc668790084d2",
+    "url": "/static/media/opm.7d31297d.png"
+  },
+  {
+    "revision": "eaedbfe35323ecfbc2844eaed950db9e",
+    "url": "/static/media/origami.eaedbfe3.svg"
+  },
+  {
+    "revision": "f4a76d61ef1bf015ca07eb5feebbeaff",
+    "url": "/static/media/tea.f4a76d61.jpg"
+  },
+  {
+    "revision": "0d357aa2988ac5bea95d62f31afd94b3",
+    "url": "/static/media/teacher.0d357aa2.jpg"
+  },
+  {
+    "revision": "6e81da9a839fd944b69ccd29b5adb2d7",
+    "url": "/static/media/thumb.6e81da9a.svg"
+  }
+]);
