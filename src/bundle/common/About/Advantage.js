de7c9f50327f1c2468bab0d4607ad4adb25910bb
kaini <53599628+Kainar01@users.noreply.github.com>
2020-01-03 21:29:11 +0600

    commet

--- src/bundle/common/About/Advantage.js
+++ src/bundle/common/About/Advantage.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import origami from '../../.././assets/origami.svg'
-import teacher from '../../.././assets/teacher.jpg'
-import level from '../../.././assets/level.jpg'
-import hands from '../../.././assets/hands.svg'
-import thumb from '../../.././assets/thumb.svg'
-import opm from '../../.././assets/opm.png'
+
 
 class Advantage extends React.Component{
     constructor(props){
@@ -61,12 +56,12 @@ class Advantage extends React.Component{
             </form>
         <form onSubmit={this.handleSubmit} method="POST" action="email.php">
             <div><label>Имя:</label></div>
-            <div><input type="text" name="myName" className="form-control" /></div>
+            <div><input type="text" name="myName" className="form-control"  /></div>
             <div><label>Почта:</label></div>
-            <div><input type="text" name="myEmail" className="form-control" required/></div>
+            <div><input type="text" name="myEmail" className="form-control" placeholder="example@gmail.com" required/></div>
             <input type="text" id="website" name="website"/>
             <div><label>Вопрос:</label></div>
-            <div><textarea cols="40" rows="5" name="myMessage" className="form-control" placeholder="kotak" value="lolol" required></textarea></div>
+            <div><textarea cols="40" rows="5" name="myMessage" className="form-control" placeholder="Вопрос который вас интересует?" required></textarea></div>
             <div className="float-right mt-2">
                 <input type="submit" value="Send" className="btn btn-primary" />
             </div>
